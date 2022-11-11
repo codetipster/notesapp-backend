10a767fc355ffc2fdea9338bbf621d73e9bf3d19
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2022-11-11 16:20:56 +0100

    add node for fetching one note resource

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //import expresss
+const { request } = require('express')
 const express = require('express')
 //store it in the app variable
 const app = express()
@@ -30,6 +31,26 @@ let notes = [
   app.get('/api/notes', (request, response) => {
     response.json(notes)
   })
+
+  //get a single unique note: handles all HTTP GET requests that are of the form /api/notes/SOMETHING
+  app.get('/api/notes/:id', (request, response) => {
+    //get the string id from the request obj by accessing the http request payload using param
+    const id = Number(request.params.id)
+    //search through the server for the note with the unique id
+    const note = notes.find(n => n.id === id)
+    // return the note as json if note is found, else return a 404 network error.
+    if(note){
+        response.json(note)
+    }else{
+        //accessing the status parameter of the response object
+        response.status(404).end()
+    }
+  })
+
+  
+
+
+
 
 
 //Tell the server on what port it should listen to for an incoming server request from client.  
