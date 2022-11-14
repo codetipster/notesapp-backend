cf5630f89e820e665379d8794a87e75861907e3e
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2022-11-14 10:42:09 +0100

    expanding our error handler to handle the likely errors from validation failure

--- index.js
+++ index.js
@@ -107,7 +107,9 @@ app.get('/api/notes', (request, response) => {
   
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
-    } 
+    } else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
+    }
   
     next(error)
   }
